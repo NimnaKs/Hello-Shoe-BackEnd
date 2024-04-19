269031cef9a1f6803c51c26cd75ea985f8c54aa7
NimnaKs <nimnakaveesha123456@gmail.com>
2024-04-19 15:31:34 +0530

    init commit

---
+++ src/main/java/com/codeventlk/helloshoemanagementsystem/HelloShoeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeventlk.helloshoemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloShoeManagementSystemApplication.class, args);
+    }
+
+}
