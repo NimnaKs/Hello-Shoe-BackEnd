46aaea80d395820c7b40f5865de27ec3529126c5
NimnaKs <nimnakaveesha123456@gmail.com>
2024-04-25 15:54:00 +0530

    Add DTOs.

--- src/main/java/com/codeventlk/helloshoemanagementsystem/dto/ItemDTO.java
+++ src/main/java/com/codeventlk/helloshoemanagementsystem/dto/ItemDTO.java
@@ -4,6 +4,7 @@ import com.codeventlk.helloshoemanagementsystem.Enum.Status;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public class ItemDTO implements SuperDTO{
     @Null(message = "Item code generate by the program")
@@ -16,5 +17,23 @@ public class ItemDTO implements SuperDTO{
     private Status status;
     @NotNull(message = "Category cannot be null")
     private String category;
+    @NotBlank(message = "Supplier code cannot be blank")
+    private String supplierCode;
+
+    @NotNull(message = "Unit Price Sale cannot be null.")
+    @PositiveOrZero(message = "Unit Price Sale cannot be negative.")
+    private Double unitPriceSale;
+
+    @NotNull(message = "Unit Price Buy cannot be null.")
+    @PositiveOrZero(message = "Unit Price Buy cannot be negative.")
+    private Double unitPriceBuy;
+
+    @NotNull(message = "Expected Profit Sale cannot be null.")
+    @PositiveOrZero(message = "Expected Profit Sale cannot be negative.")
+    private Double expectedProfit;
+
+    @NotNull(message = "Profit Margin cannot be null.")
+    @PositiveOrZero(message = "profit Margin cannot be negative.")
+    private Double profitMargin;
 
 }
